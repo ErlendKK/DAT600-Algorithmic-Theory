21c12243d368f2f743ac7f02e397d5f6c6666ffa
ErlendKK <136587757+ErlendKK@users.noreply.github.com>
2024-04-18 18:29:36 +0200

    Update Assignment4.py

--- Assignment 4/Assignment4.py
+++ Assignment 4/Assignment4.py
@@ -25,7 +25,78 @@ y_bounds = (0, None)
 
 def solve_problem_one():
     result = linprog(c, A_ub=A_ub, b_ub=b_ub, bounds=[x_bounds, y_bounds], method='highs')
+    print("Problem 1")
     print(f"Optimal verdi: {-result.fun}")
-    print(f"x-verdier: {result.x}")
+    print(f"x-verdier: {result.x}\n")
 
 solve_problem_one()
+
+
+##############
+# Problem 2a #
+############## 
+
+import pulp
+import numpy as np
+
+G = np.array([
+    [0, 14, 25,  0,  0,  0,  0], # s
+    [0,  0,  0, 21,  0,  0,  0], # V1
+    [0,  0,  0,  3,  0,  7,  0], # V2
+    [0,  6, 13,  0, 10,  5,  0], # V3
+    [0,  0,  0,  0,  0,  0, 20], # V4
+    [0,  0,  0,  0, 10,  0, 10], # V5
+    [0,  0,  0,  0,  0,  0,  0]  # t
+])
+
+
+def solve_problem_two_a(G):
+    num_nodes = G.shape[0]
+    edges = [(i, j) for i in range(num_nodes) for j in range(num_nodes) if G[i, j] > 0]
+    prob = pulp.LpProblem("MinimumCut", pulp.LpMinimize)
+
+    # source set (1) or sink set (0) 
+    in_source_set = pulp.LpVariable.dicts("in_source_set", range(num_nodes), cat=pulp.LpBinary)
+    prob += in_source_set[0] == 1
+    prob += in_source_set[num_nodes - 1] == 0
+
+    # included/excluded from the cut -> {1,0}
+    x = pulp.LpVariable.dicts("x", edges, cat=pulp.LpBinary)
+
+
+    # Objective Function: 
+    # Minimize the sum of the capacities of the cut edges
+
+    prob += pulp.lpSum([G[i][j] * x[(i, j)] for (i, j) in edges])
+
+
+    for (i, j) in edges:
+        prob += x[(i, j)] >= in_source_set[i] - in_source_set[j]
+
+
+    status = prob.solve(pulp.PULP_CBC_CMD(msg=False))
+    cut_edges = [(i, j) for (i, j) in edges if pulp.value(x[(i, j)]) == 1]
+
+    print(f"Problem 2a")
+    print(f"Minimum Cut Value: {pulp.value(prob.objective)}")
+    print(f"Edges in the cut: {cut_edges}\n")
+
+solve_problem_two_a(G)
+
+
+##############
+# Problem 2b #
+############## 
+
+from scipy.sparse import csr_matrix
+from scipy.sparse.csgraph import maximum_flow
+
+def solve_problem_two_b(G):
+    graph = csr_matrix(G)
+    source = 0
+    sink = len(G) - 1
+    result = maximum_flow(graph, source, sink)
+    print(f"Problem 2b")
+    print(f"Maximum flow: {result.flow_value}") 
+
+solve_problem_two_b(G)
